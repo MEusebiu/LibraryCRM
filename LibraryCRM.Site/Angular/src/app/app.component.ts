015834cd95256304384e4a2b6521fd891d4e440b
Eusebiu Mihaes <eusebiu.mihaes@duk-tech.com>
2019-01-11 12:43:28 +0200

    Web Api brings [Object]'s to Angular

--- LibraryCRM.Site/Angular/src/app/app.component.ts
+++ LibraryCRM.Site/Angular/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../app/services/book.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'LibraryAngular';
+export class AppComponent implements OnInit {
+  constructor(private dataService: BookService) { }
+
+  books: any;
+
+  ngOnInit() {
+    this.dataService.getBooks().subscribe((param) => (this.books = param));
+  };
+}
+
+interface Book {
+  Author: string,
+  Title: string,
+  Year: Date;
 }
---
+++ LibraryCRM.Site/Angular/src/app/services/book.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class BookService {
+
+  constructor(private http: HttpClient) { }
+
+  getBooks() {
+    return this.http.get('http://localhost:8080/api/book').pipe(map(res => res));
+  }
+}
