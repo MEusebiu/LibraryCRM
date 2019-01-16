035c9dea71a4acab99314a3b9e8b29d978ea687d
Eusebiu Mihaes <eusebiu.mihaes@duk-tech.com>
2019-01-16 13:36:48 +0200

    Installed jQuery and Bootstrap - Tabs work

--- LibraryCRM.CoreWeb/AngularApp/src/app/app.module.ts
+++ LibraryCRM.CoreWeb/AngularApp/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'; 
 
+//My components
 import { BookComponent } from './components/book/book.component';
 import { AuthorComponent } from './components/author/author.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+
+//My services
+import { AuthorService } from './services/author.service';
+import { BookService } from './services/book.service';
 
 @NgModule({
   declarations: [
@@ -19,9 +25,10 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BookService, AuthorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
