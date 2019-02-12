c2b66aafa49f739f2bfc303a0cf52c56acfd0c91
Eusebiu Mihaes <eusebiu.mihaes@duk-tech.com>
2019-02-12 09:47:27 +0200

    Repository and IRepository added

---
+++ LibraryCRM.Repository/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCRM.Repository.Repository
+{
+    public interface IRepository<T> : IDisposable
+    {
+        void Create(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        IQueryable<T> GetAll();
+        IQueryable<T> GetEntity(int id);
+        void SaveChanges();
+    }
+}
---
+++ LibraryCRM.Repository/Repository/Repository.cs
@@ -0,0 +1,61 @@
+﻿using LibraryCRM.DataContext.DbContext;
+using LibraryCRM.DataContext.Entities;
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+
+namespace LibraryCRM.Repository.Repository
+{
+    public class Repository<T> : IRepository<T> where T : Entity
+    {
+        private DbSet<T> _dbSet;
+        protected LibraryDbContext _dbContext;
+
+        public Repository(LibraryDbContext dbContext)
+        {
+            _dbSet = dbContext.Set<T>();
+            _dbContext = dbContext;
+        }
+
+        public void Create(T entity)
+        {
+            _dbSet.Add(entity);
+            SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+            SaveChanges();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public IQueryable<T> GetEntity(int id)
+        {
+            return _dbSet.Where(x => x.Id == id);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            SaveChanges();
+        }
+
+        public void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+        }
+
+    }
+
+}
