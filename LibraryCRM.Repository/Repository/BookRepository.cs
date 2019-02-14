ccbbc26a012c1277e3673d17762b3d55b844a44f
Eusebiu Mihaes <eusebiu.mihaes@duk-tech.com>
2019-02-14 13:19:30 +0200

    Added BookRepo

---
+++ LibraryCRM.Repository/Repository/BookRepository.cs
@@ -0,0 +1,38 @@
+﻿using LibraryCRM.DataContext.DbContext;
+using LibraryCRM.DataContext.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCRM.Repository.Repository
+{
+    public class BookRepository : Repository<Book>, IBookRepository
+    {
+        public BookRepository(LibraryDbContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        public IList<Book> GetAllDetails()
+        {
+            return _dbContext.Books.Include("Author").Include("Genre").Include("Reviews").ToList();
+        }
+
+        public Dictionary<int, string> GetBookAuthors()
+        {
+            return _dbContext.Authors.ToDictionary(x => x.Id, x => x.FirstName);
+        }
+
+        public Dictionary<int, string> GetBookGenres()
+        {
+            return _dbContext.Genres.ToDictionary(x => x.Id, x => x.Name);
+        }
+
+        public IList<Review> GetBookReviews()
+        {
+            return _dbContext.Reviews.ToList();
+        }
+    }
+}
---
+++ LibraryCRM.Repository/Repository/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using LibraryCRM.DataContext.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCRM.Repository.Repository
+{
+    public interface IBookRepository : IRepository<Book>
+    {
+        IList<Book> GetAllDetails();
+        Dictionary<int, string> GetBookAuthors();
+        Dictionary<int, string> GetBookGenres();
+        IList<Review> GetBookReviews();
+    }
+}
